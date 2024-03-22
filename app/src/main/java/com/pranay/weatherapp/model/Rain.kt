ab08464ef19edde64e94d7b2727d07a807a0b2ab
pranay4saini <pranay4saini@gmail.com>
2024-03-23 01:40:09 +0530

    created various model classes

---
+++ app/src/main/java/com/pranay/weatherapp/model/Rain.kt
@@ -0,0 +1,5 @@
+package com.pranay.weatherapp.model
+
+data class Rain(
+    val `1h`: Double
+)
---
+++ app/src/main/java/com/pranay/weatherapp/model/Weather.kt
@@ -0,0 +1,10 @@
+package com.pranay.weatherapp.model
+data class Weather(
+    val current: Current,
+    val daily: List<Daily>,
+    val hourly: List<Hourly>,
+    val lat: Double,
+    val lon: Double,
+    val timezone: String,
+    val timezone_offset: Int
+)
